0787f449fdceaa8b574e8d733ca05abedf96c5d1
DevelopWoo <kksw7000@naver.com>
2026-04-17 21:14:48 +0900

    chore : useParams 콘솔 로직 삭제

--- src/pages/UpdatePage/StudyUpdate.jsx
+++ src/pages/UpdatePage/StudyUpdate.jsx
@@ -17,7 +17,6 @@ import { useNavigate, useParams } from 'react-router-dom';
 const StudyUpdate = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  console.log('params id:', id);
 
   const [nickname, setNickname] = useState('');
 
