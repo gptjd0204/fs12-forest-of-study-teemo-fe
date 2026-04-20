e736d5d1b43a323fa52398bf8da1979d3b68d068
gptjd0204 <gptjd0204@naver.com>
2026-04-20 15:43:29 +0900

    refactor: TimerFocus에 있는 State들 커스텀 훅으로 분리

---
+++ src/hooks/useFetchTimer.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import { createTimer, getTimer } from '../services/TimerService';
+import { getTotalPoint } from '../services/PointService';
+
+/*------------------------------------------
+     타이머 데이터를 받아오는 커스텀 Hook
+-------------------------------------------*/
+const useFetchTimer = (studyId) => {
+  const [targetDuration, setTargetDuration] = useState(0);
+  const [timerCount, setTimerCount] = useState(targetDuration);
+  const [timerStatus, setTimerStatus] = useState('CANCELED');
+  const [totalPoint, setTotalPoint] = useState(0);
+  const [title, setTitle] = useState('');
+
+  // 타이머 초기화 함수
+  const initTimer = () => {
+    setTargetDuration(1500000);
+    setTimerCount(1500000);
+    setTimerStatus('CANCELED');
+  };
+
+  useEffect(() => {
+    try {
+      const fetchTimer = async () => {
+        const data = await getTimer(studyId);
+        const timerData = data.timer;
+        const total = await getTotalPoint(studyId);
+        setTitle(data.title);
+        if (!timerData) {
+          initTimer();
+          await createTimer(studyId);
+          return;
+        }
+
+        setTotalPoint(total);
+        setTargetDuration(timerData.targetDuration);
+        setTimerStatus(timerData.status);
+        // 타이머 남은 시간 계산 로직
+        if (timerData.status === 'IN_PROGRESS') {
+          const now = Date.now();
+          const lastStartedAt = new Date(timerData.lastStartedAt);
+
+          const timeDiff = now - lastStartedAt;
+          const totalElapsedTime = timerData.elapsedTime + timeDiff;
+          const remainingTime = timerData.targetDuration - totalElapsedTime;
+
+          if (remainingTime < 0) {
+            setTimerStatus('COMPLETED');
+            setTimerCount(totalElapsedTime - timerData.targetDuration + 700);
+          } else {
+            setTimerCount(remainingTime + 700);
+          }
+        } else {
+          setTimerCount(timerData.targetDuration - timerData.elapsedTime + 700);
+        }
+      };
+
+      fetchTimer();
+    } catch (error) {
+      console.error(error);
+    }
+  }, [studyId]);
+
+  return {
+    targetDuration,
+    setTargetDuration,
+    timerCount,
+    setTimerCount,
+    timerStatus,
+    setTimerStatus,
+    totalPoint,
+    setTotalPoint,
+    title,
+  };
+};
+
+export default useFetchTimer;
---
+++ src/hooks/useTimer.js
@@ -0,0 +1,108 @@
+import { useEffect, useRef, useState } from 'react';
+import {
+  updateComplete,
+  updatePause,
+  updateReset,
+  updateStart,
+} from '../services/TimerService';
+
+/*------------------------------------------
+        타이머를 설정하는 커스텀 Hook
+-------------------------------------------*/
+const useTimer = (
+  studyId,
+  targetDuration,
+  setTargetDuration,
+  setTotalPoint,
+  timerStatus,
+  setTimerStatus,
+  timerCount,
+  setTimerCount,
+) => {
+  const [toast, setToast] = useState({ show: false, type: '', msg: '' });
+  const timerRef = useRef();
+
+  // 타이머 초기화 함수
+  const initTimer = () => {
+    setTargetDuration(1500000);
+    setTimerCount(1500000);
+    setTimerStatus('CANCELED');
+  };
+
+  // 토스트 메시지 출력
+  useEffect(() => {
+    setTimeout(() => {
+      setToast({ show: false, type: '', msg: '' });
+    }, 3000);
+  }, [toast]);
+
+  // 타이머 시간 출력
+  useEffect(() => {
+    if (timerStatus === 'COMPLETED') {
+      timerRef.current = setInterval(() => {
+        setTimerCount((prev) => prev + 1000);
+      }, 1000);
+    }
+    if (timerStatus === 'IN_PROGRESS') {
+      timerRef.current = setInterval(() => {
+        setTimerCount((prev) => {
+          if (prev - 1000 < 1000) {
+            setTimerStatus('COMPLETED');
+          }
+          return prev - 1000;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, [timerStatus, setTimerCount, setTimerStatus]);
+
+  /*-----------------------------------------------------
+            타이머 조작 핸들러(시작, 일시정지, 리셋)
+    ------------------------------------------------------*/
+  const timerStartHandler = async () => {
+    setTimerStatus('IN_PROGRESS');
+    await updateStart(studyId);
+  };
+
+  const timerPauseHandler = async () => {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+    setTimerStatus('PAUSED');
+    setToast({ show: true, type: 'error', msg: '집중이 중단되었습니다.' });
+    await updatePause(studyId);
+  };
+
+  const timerResetHandler = async () => {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+    setTimerStatus('CANCELED');
+    setTimerCount(targetDuration);
+    await updateReset(studyId);
+  };
+
+  const timerCompleteHandler = async () => {
+    const points = await updateComplete(studyId);
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+    setTotalPoint((prev) => prev + points);
+    initTimer();
+    setToast({
+      show: true,
+      type: 'success',
+      msg: `${points}포인트를 획득했습니다!`,
+    });
+  };
+
+  return {
+    toast,
+    timerStartHandler,
+    timerPauseHandler,
+    timerResetHandler,
+    timerCompleteHandler,
+  };
+};
+
+export default useTimer;
