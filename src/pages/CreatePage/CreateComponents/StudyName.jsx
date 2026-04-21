6fe33aad74bf820836d7768a3015b5c9ea89b963
DevelopWoo <kksw7000@naver.com>
2026-04-21 16:04:18 +0900

    fix : 닉네임, 이름, 소개 최대글자수 수정

--- src/pages/CreatePage/CreateComponents/StudyName.jsx
+++ src/pages/CreatePage/CreateComponents/StudyName.jsx
@@ -7,10 +7,13 @@ const StudyName = ({ title, setTitle }) => {
 
   const validateTitle = (value) => {
     if (!value) {
-      return '*스터디 이름을 입력해주세요';
+      return '스터디 이름을 입력해주세요';
     }
     if (value !== value.trim()) {
       return '앞뒤 공백은 사용할 수 없습니다.';
+    }
+    if (value.length > 20) {
+      return '스터디 이름은 20자 이하이어야 합니다.';
     }
     return '';
   };
