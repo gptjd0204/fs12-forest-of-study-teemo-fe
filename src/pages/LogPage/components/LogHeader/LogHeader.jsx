a921ad59fcf6d7f8a35f33edef22c0daaf90ac44
NAYA3 <popcorn7274@gmail.com>
2026-04-21 16:40:51 +0900

    style: CurrentTime 피그마 UI대로 수정

--- src/pages/LogPage/components/LogHeader/LogHeader.jsx
+++ src/pages/LogPage/components/LogHeader/LogHeader.jsx
@@ -7,64 +7,64 @@ const LogHeader = ({ id, study, logType, setLogType }) => {
 
   return (
     <div className={styles.topwrapper}>
-        {/** 스터디이름, 링크 */}
-        <div className={styles.top}>
-          <h1>
-            {nickname}의 {title}
-          </h1>
-          <div className={styles.linkContainer}>
-            <LinkButton 
-              className={styles.linkButton}
-              text="오늘의 습관" 
-              url={`/${id}/habit`}
-            />
-            <LinkButton  
-              className={styles.linkButton}
-              text="오늘의 집중" 
-              url={`/${id}/focus`}
-            />
-            <LinkButton 
-              className={styles.linkButton}
-              text="홈" 
-              url={`/${id}/detail`}
-            />
-          </div>
-        </div>
-        
-        <div className={styles.dateWrapper}>
-          {/** 현재 시간 */}
-          <div className={styles.timeContainer}>
-            <CurrentTime />
-          </div>
-
-          {/** 라디오 버튼 */}
-          <div className={styles.radioBox}>
-            <label 
-              className={`${styles.radioBoxItem} ${
-                logType === "focus" ? styles.active : ""
-              }`}>
-              <input
-                type="radio"
-                value="focus"
-                checked={logType === "focus"}
-                onChange={(e) => setLogType(e.target.value)}
-              />
-              집중 시간
-            </label>
-            <label className={`${styles.radioBoxItem} ${
-              logType === "point" ? styles.active : ""
-            }`}>
-              <input 
-                type="radio"
-                value="point"
-                checked={logType === "point"}
-                onChange={(e) => setLogType(e.target.value)}
-              />
-              포인트
-            </label>
-          </div>       
+      {/** 스터디이름, 링크 */}
+      <div className={styles.top}>
+        <h1>
+          {nickname}의 {title}
+        </h1>
+        <div className={styles.linkContainer}>
+          <LinkButton 
+            className={styles.linkButton}
+            text="오늘의 습관" 
+            url={`/${id}/habit`}
+          />
+          <LinkButton  
+            className={styles.linkButton}
+            text="오늘의 집중" 
+            url={`/${id}/focus`}
+          />
+          <LinkButton 
+            className={styles.linkButton}
+            text="홈" 
+            url={`/${id}/detail`}
+          />
         </div>
       </div>
+      
+      <div className={styles.dateWrapper}>
+        {/** 현재 시간 */}
+        <div className={styles.timeContainer}>
+          <CurrentTime />
+        </div>
+
+        {/** 라디오 버튼 */}
+        <div className={styles.radioBox}>
+          <label 
+            className={`${styles.radioBoxItem} ${
+              logType === "focus" ? styles.active : ""
+            }`}>
+            <input
+              type="radio"
+              value="focus"
+              checked={logType === "focus"}
+              onChange={(e) => setLogType(e.target.value)}
+            />
+            집중 시간
+          </label>
+          <label className={`${styles.radioBoxItem} ${
+            logType === "point" ? styles.active : ""
+          }`}>
+            <input 
+              type="radio"
+              value="point"
+              checked={logType === "point"}
+              onChange={(e) => setLogType(e.target.value)}
+            />
+            포인트
+          </label>
+        </div>       
+      </div>
+    </div>
   );
 };
 
