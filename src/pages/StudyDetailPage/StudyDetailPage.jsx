fe4299a144284a2a9852ff17c52d713e0be72d7b
yoonsojung <mila030403@hansung.ac.kr>
2026-04-21 10:34:34 +0900

    fix : 스터디 생성 후 최근 조회에 추가

--- src/pages/StudyDetailPage/StudyDetailPage.jsx
+++ src/pages/StudyDetailPage/StudyDetailPage.jsx
@@ -10,6 +10,7 @@ import Modals from './components/Modals/Modals';
 import Toast from '../../components/Toast/Toast';
 
 import { getStudyDetail } from '../../services/StudyDetailService.js';
+import { saveRecentStudy } from '../../services/StudyService.js';
 
 import styles from './StudyDetailPage.module.css';
 
@@ -49,6 +50,7 @@ const StudyDetailPage = () => {
       }
 
       setStudy(data);
+      saveRecentStudy(data);
     } catch (error) {
       console.log(error);
       throw error;
